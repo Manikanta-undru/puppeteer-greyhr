56dd17e9bceed62b929b7941dadc061a5c5da186
Manikanta Undru <manikanta@blockoville.com>
2024-02-11 16:11:39 +0530

    modified brower args

--- greyHr.js
+++ greyHr.js
@@ -4,8 +4,9 @@ const signInIntoGreyHr = async (req, res) => {
     const signInText = 'You have Successfully Signed in'
     const signOutText = 'You have Successfully Signed out'
     const browser = await puppeteer.launch({
-      executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH: puppeteer.executablePath(),
-      args: ['--no-sandbox', '--disable-setuid-sandbox','--single-process', '--no-zygote'],
+    executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH: puppeteer.executablePath(),
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    // args: ['--no-sandbox', '--disable-setuid-sandbox','--single-process', '--no-zygote'],
     });
     console.log('Browser launched');
     let successText = null;
