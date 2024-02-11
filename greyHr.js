64d61952d1865cd2756a9ac6576783b8dd2cdd26
Manikanta Undru <manikanta@blockoville.com>
2024-02-11 17:03:50 +0530

    modified wait times

--- greyHr.js
+++ greyHr.js
@@ -14,11 +14,14 @@ const signInIntoGreyHr = async (req, res) => {
     while (!successText && retryCount < 3) {
         try {
             const page = await browser.newPage();
+            console.log('v1.2.0');
             console.log('New page created');
             await page.goto('https://sunovaatech.greythr.com/');
-            console.log('Navigated to GreyHR website');
+            console.log('Navigating to GreyHR website');
+            await page.waitForNetworkIdle();
+            console.log('Network idle');
 
-            await page.waitForSelector('#username', { timeout: 10000 }); // Wait until input with id 'username' appears
+            await page.waitForSelector('#username', { timeout: 50000 }); // Wait until input with id 'username' appears
             console.log('Username input field found');
             await page.type('#username', process.env.GREYHR_USERNAME); // Type in username
             console.log('Username typed');
@@ -26,21 +29,24 @@ const signInIntoGreyHr = async (req, res) => {
             console.log('Password typed');
             await page.click('button[type="submit"]'); // Click on submit button
             console.log('Submit button clicked');
-            await page.waitForSelector('.home-dashboard', { timeout: 10000 }); // Wait until the class 'home-dashboard' appears in the DOM
+            await page.waitForNavigation({ waitUntil: 'networkidle0' }); // Wait until the page navigates to the next page
+            console.log('Navigated to the next page');
+            await page.waitForNetworkIdle();
+            await page.waitForSelector('.home-dashboard', { timeout: 50000 }); // Wait until the class 'home-dashboard' appears in the DOM
             console.log('Dashboard loaded');
-            await page.waitForSelector('gt-button', { timeout: 10000 }); // Wait until the custom element 'gt-button' with class name 'hydrate' and attribute 'shade' equal to 'primary' appears in the DOM
+            await page.waitForSelector('gt-button', { timeout: 50000 }); // Wait until the custom element 'gt-button' with class name 'hydrate' and attribute 'shade' equal to 'primary' appears in the DOM
             console.log('gt-button element found');
             await page.waitForSelector('gt-home-dashboard > div > div:nth-child(2) > gt-component-loader > gt-attendance-info > div > div', { timeout: 10000 }); // Wait until the custom element 'gt-attendance-info' appears in the DOM
             const gtButton = await page.$('gt-home-dashboard > div > div:nth-child(2) > gt-component-loader > gt-attendance-info > div > div ');
             console.log('gt-attendance-info element found');
-            await gtButton.waitForSelector('div.btn-container > gt-button', { timeout: 10000 }); // Wait until the custom element 'gt-button' with class name 'hydrate' and attribute 'shade' equal to 'primary' appears in the DOM
+            await gtButton.waitForSelector('div.btn-container > gt-button', { timeout: 50000 }); // Wait until the custom element 'gt-button' with class name 'hydrate' and attribute 'shade' equal to 'primary' appears in the DOM
             const btnContainer = await gtButton.$('div.btn-container > gt-button');
             console.log('gt-button element found');
             await btnContainer.click('>>> button'); // Click on the sign in button
             console.log('sign in/out Button clicked');
-            await page.waitForSelector('.alert-success', { timeout: 20000 }); // Wait until the success alert appears
+            await page.waitForSelector('.alert-success', { timeout: 50000 }); // Wait until the success alert appears
             console.log('Success alert appeared');
-            await page.waitForSelector('.alert-success p', { timeout: 20000 }); // Wait until the paragraph inside the success alert appears
+            await page.waitForSelector('.alert-success p', { timeout: 50000 }); // Wait until the paragraph inside the success alert appears
             console.log('Paragraph inside success alert appeared');
             successText = await page.$eval('.alert-success p', element => element.textContent); // Get the text content of the paragraph inside the success alert
             console.log('Success text obtained:', successText);
