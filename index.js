983a376c42b04c00e45401cf70bb8c6017af8aaa
Manikanta Undru <manikanta@blockoville.com>
2024-02-11 18:21:20 +0530

    added date to runs

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+const datesToRun = JSON.parse(process.env.DATES_TO_RUN);
+
 app.get('/', (req, res) => {
   res.send('Hello, Puppeteer server is running!');
 });
 
 app.get('/signIn', (req, res) => {
-   signInIntoGreyHr(req, res);
-  });
-
+  console.log('Date to run:', datesToRun);
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  console.log('currentMonth:', currentMonth);
+  console.log('currentDate:', currentDate.getDate());
+  if (datesToRun[currentMonth].includes(currentDate.getDate())) {
+    console.log('Date is in the list');
+    signInIntoGreyHr(req, res);
+  } else {
+    console.log('Date is not in the list');
+    res.send('Date is not in the list');
+  }
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
